d46cef9a355a9c524d38e5d33832604848f72bb4
Jonathan Harker <code@gentlydownthe.net>
2014-10-11 12:37:27 -0700

    Move uber::create_index_html to its own file

---
+++ manifests/create_index_html.pp
@@ -0,0 +1,29 @@
+# doesn't work right now.
+define uber::create_index_html (
+  $public_url,
+  $event_name,
+  $year,
+) {
+  if ! defined(Uber::Concat['/var/www/index.html']) {
+    concat { '/var/www/index.html':
+    }
+
+    concat::fragment { "uberindexfilehtml_header_${name}":
+      target  => '/var/www/index.html',
+      content => "<html><body><h1>Ubersystem</h1><br/>",
+      order   => '01',
+    }
+
+    concat::fragment { "uberindexfilehtml_footer_${name}":
+      target  => '/var/www/index.html',
+      content => "</body></html>",
+      order   => '03',
+    }
+  }
+
+  concat::fragment { "uberindexfilehtml_${name}":
+    target  => '/var/www/index.html',
+    content => "<p><a href=\"${public_url}\">${event_name} ${year} Ubersystem</a></p>",
+    order   => '02',
+  }
+}
