aa59949d6f2a56268ad33f8b98ff8b1eab457270
Jonathan Harker <code@gentlydownthe.net>
2014-10-11 12:59:49 -0700

    Move uber::dbreplicationmaster to its own file

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -367,31 +367,3 @@ define uber::allow-replication-from-ip (
     port  => 5432,
   }
 }
-
-define uber::dbreplicationmaster (
-  $dbname,
-  $replication_user = 'replicator',
-  $replication_password,
-  $slave_ips,
-) {
-  postgresql::server::role { $replication_user:
-    password_hash => postgresql_password($replication_user, $replication_password),
-    replication   => true,
-    #notify        => Postgresql::Server::Config_Entry['wal_level'],
-    subscribe      => Postgresql::Server::Db["${dbname}"]
-  }
-
-  postgresql::server::config_entry {
-     # 'listen_address':       value => "*";
-     'wal_level':            value => 'hot_standby';
-     'max_wal_senders':      value => '3';
-     'checkpoint_segments':  value => '8';
-     'wal_keep_segments':    value => '8';
-  }
-
-  uber::allow-replication-from-ip { $slave_ips:
-    dbname   => $dbname,
-    username => $replication_user,
-  }
-}
-
