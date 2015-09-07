00539dbcbc83709e08b37d10285b6d6b220e32c3
Dominic Cerquetti <dom@magfest.org>
2015-09-07 19:35:55 -0400

    update self-signed keys to not have same CommonName
    openssl thinks that anything with the same CommonName is self-signed which is not what we want at all here.

--- files/generate-CA-and-clientcerts.sh
+++ files/generate-CA-and-clientcerts.sh
@@ -23,7 +23,12 @@ subj_state=$3
 subj_location=$4
 subj_organization=$5
 subj_common_name=$6
-subj="/C=$subj_country_code/ST=$subj_state/L=$subj_location/O=$subj_organization/CN=$subj_common_name"
+
+subj="/C=$subj_country_code/ST=$subj_state/L=$subj_location/O=$subj_organization/CN"
+
+subj_ca="$subj=$subj_common_name"
+subj_server="$subj=$subj_common_name-server"
+subj_client="$subj=$subj_common_name-client"
 
 ssl_ca_key=${prefix}-ca.key
 ssl_ca_crt=${prefix}-ca.crt
@@ -35,18 +40,18 @@ ssl_client_csr=${prefix}-client.csr
 ssl_client_crt=${prefix}-client.crt
 
 # create CA certificate
-openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "$subj" -keyout $ssl_ca_key -out $ssl_ca_crt
+openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "$subj_ca" -keyout $ssl_ca_key -out $ssl_ca_crt
 
 # create server key and signing request
 openssl genrsa -out $ssl_server_key 1024
-openssl req -new -nodes -subj "$subj" -key $ssl_server_key -out $ssl_server_csr
+openssl req -new -nodes -subj "$subj_server" -key $ssl_server_key -out $ssl_server_csr
 
 # create server certificate signed by our CA
 openssl x509 -req -days 365 -in $ssl_server_csr -CA $ssl_ca_crt -CAkey $ssl_ca_key -set_serial 01 -out $ssl_server_crt
 
 # create client key and signing request
 openssl genrsa -out $ssl_client_key 1024
-openssl req -new -nodes -subj "$subj" -key $ssl_client_key -out $ssl_client_csr
+openssl req -new -nodes -subj "$subj_client" -key $ssl_client_key -out $ssl_client_csr
 
 # create client cert signed by our CA
 openssl x509 -req -days 365 -in $ssl_client_csr -CA $ssl_ca_crt -CAkey $ssl_ca_key -set_serial 01 -out $ssl_client_crt
