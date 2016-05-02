d79fda70e12479b4e1480d140b8309bcbe507347
Dominic Cerquetti <binary1230@gmail.com>
2016-05-02 15:30:32 -0400

    enable band / panels plugin for puppet - hook up minimum subset of INI values needed to enable the band/panels plugins - band plugin depends on panels plugin for scheduling - more INI breakout is needed, but this is a good scaffolding for starting that work

---
+++ manifests/plugin_panels.pp
@@ -0,0 +1,25 @@
+# TODO: probably should move this file into its own puppet module
+# TODO: need to make config handling more generic, this should work with ANY plugin
+
+class uber::plugin_panels (
+  $git_repo = "https://github.com/magfest/panels",
+  $git_branch = "master",
+  $hide_schedule = true,
+  $expected_response = undef,
+  $event_location = undef,
+  $panel_rooms = undef,
+  $panel_app_deadline = undef,
+) {
+  uber::repo { "${uber::plugins_dir}/panels":
+    source   => $git_repo,
+    revision => $git_branch,
+    require  => File["${uber::plugins_dir}"],
+  }
+
+  file { "${uber::plugins_dir}/panels/development.ini":
+    ensure  => present,
+    mode    => 660,
+    content => template('uber/panels-development.ini.erb'),
+    require => [ Uber::Repo["${uber::plugins_dir}/panels"] ],
+  }
+}
