46827ff9333e6a72807cd9a77c2b7299e7bc670f
Victoria Earl <kitsuta@gmail.com>
2016-05-12 16:10:31 +0200

    Add deadlines to bands plugin config Since the deadlines are likely to change, events can now set them.

--- manifests/plugin_bands.pp
+++ manifests/plugin_bands.pp
@@ -4,6 +4,14 @@
 class uber::plugin_bands (
   $git_repo = "https://github.com/magfest/bands",
   $git_branch = "master",
+  $band_panel_deadline = undef,
+  $band_bio_deadline = undef,
+  $band_agreement_deadline = undef,
+  $band_w9_deadline = undef,
+  $band_merch_deadline = undef,
+  $band_charity_deadline = undef,
+  $band_badge_deadline = undef,
+  $stage_agreement_deadline = undef,
   $stage_agreement_deadline = undef,
 ) {
   uber::repo { "${uber::plugins_dir}/bands":
