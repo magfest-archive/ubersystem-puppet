a922496a70f2bc4e8f2a955fa7206cf51f549f51
Dominic Cerquetti <binary1230@gmail.com>
2016-08-18 21:01:17 -0400

    add puppetiziation of mivs-related dates INI settings

---
+++ manifests/plugin_mivs.pp
@@ -0,0 +1,27 @@
+# TODO: probably should move this file into its own puppet module
+# TODO: need to make config handling more generic, this should work with ANY plugin
+
+class uber::plugin_mivs (
+  $git_repo = "https://github.com/magfest/mivs",
+  $git_branch = "master",
+
+  # INI settings below
+  $round_one_deadline = undef,
+  $round_two_deadline = undef,
+  $judging_deadline = undef,
+  $round_two_complete = undef,
+  $mivs_confirm_deadline = undef,
+) {
+  uber::repo { "${uber::plugins_dir}/mivs":
+    source   => $git_repo,
+    revision => $git_branch,
+    require  => File["${uber::plugins_dir}"],
+  }
+
+  file { "${uber::plugins_dir}/mivs/development.ini":
+    ensure  => present,
+    mode    => 660,
+    content => template('uber/mivs-development.ini.erb'),
+    require => [ Uber::Repo["${uber::plugins_dir}/mivs"] ],
+  }
+}
