cf6159a964cfce310de956a31c81e6f924c71324
Dominic Cerquetti <binary1230@gmail.com>
2016-08-19 10:03:23 -0400

    add support for blacklisting IP addresses via firewall - needed becuase we're starting to see weird stuff coming in - eventually we'll merge this with techops's list but for now this'll sole some immediate stuff

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -4,6 +4,7 @@ class uber::firewall (
   $socket_port = hiera('uber::socket_port'),
   $http_port = '80',
   $open_backend_port = false,
+  $blacklisted_ips = hiera('uber::firewall_blacklisted_ips')
 ) {
   if !hiera('debugONLY_dont_init_python_or_git_repos_or_plugins') {
     include ufw
@@ -25,5 +26,10 @@ class uber::firewall (
     ufw::allow { "${title}-http":
       port => $http_port,
     }
+
+    $rule_hash = generate_resource_hash($blacklisted_ips, 'ip', 'blacklist deny from IP ')
+
+    $rule_defaults = {}
+    create_resources(ufw::deny, $rule_hash, $rule_defaults)
   }
 }
