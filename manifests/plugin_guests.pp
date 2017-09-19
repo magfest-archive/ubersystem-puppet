983375596dd6efcb34dd5332f1623a0bbcc9445e
Rob Ruana <rob@robruana.com>
2017-09-19 17:20:47 -0400

    Fixes the git repo regex for bands to guests puppet rename

--- manifests/plugin_guests.pp
+++ manifests/plugin_guests.pp
@@ -45,7 +45,7 @@ class uber::plugin_guests (
   file_line { "guests_git_config":
     path   => "${uber::plugins_dir}/guests/.git/config",
     line   => "        url = $git_repo",
-    match  => "^        url = https://github.com/magfest/.*",
+    match  => "        url = https:\/\/github.com\/magfest\/.*",
     before => Uber::Repo["${uber::plugins_dir}/guests"],
   }
 
