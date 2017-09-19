2d3eef6d298518f476ab2609df9bb8a65af9111b
Victoria Earl <kitsuta@gmail.com>
2017-09-19 13:25:36 -0400

    Rename bands plugin to guests Goes with https://github.com/magfest/guests/pull/99

--- manifests/plugin_guests.pp
+++ manifests/plugin_guests.pp
@@ -1,16 +1,16 @@
 # TODO: probably should move this file into its own puppet module
 # TODO: need to make config handling more generic, this should work with ANY plugin
 
-class uber::plugin_bands (
-  $git_repo = "https://github.com/magfest/bands",
+class uber::plugin_guests (
+  $git_repo = "https://github.com/magfest/guests",
   $git_branch = "master",
 
   # INI settings below
   $band_email = undef,
   $band_email_signature = undef,
-  $require_dedicated_band_table_presence = true,
+  $require_dedicated_guest_table_presence = true,
 
-  $band_merch_enums = undef,
+  $guest_merch_enums = undef,
 
   $auction_start = '',
   $band_panel_deadline = '',
@@ -22,16 +22,16 @@ class uber::plugin_bands (
   $band_badges_deadline = '',
   $band_stage_plot_deadline = '',
 ) {
-  uber::repo { "${uber::plugins_dir}/bands":
+  uber::repo { "${uber::plugins_dir}/guests":
     source   => $git_repo,
     revision => $git_branch,
     require  => File["${uber::plugins_dir}"],
   }
 
-  file { "${uber::plugins_dir}/bands/development.ini":
+  file { "${uber::plugins_dir}/guests/development.ini":
     ensure  => present,
     mode    => 660,
-    content => template('uber/bands-development.ini.erb'),
-    require => [ Uber::Repo["${uber::plugins_dir}/bands"] ],
+    content => template('uber/guests-development.ini.erb'),
+    require => [ Uber::Repo["${uber::plugins_dir}/guests"] ],
   }
 }
