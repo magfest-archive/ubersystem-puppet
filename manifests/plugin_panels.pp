3e277a6434dd24579edcac9873e7a465e24f59e5
Rob Ruana <rob@robruana.com>
2017-12-28 21:41:44 -0500

    Puppetizes panels_twilio_sid & panels_twilio_token (#186)
    * Puppetizes panels_twilio_sid & panels_twilio_token
    
    * Puppetizes panels_twilio_sid & panels_twilio_token
    
    * Adds panels_ prefix to twilio vars
    
    * Adds attractions_email & panels_twilio_number vars
    
    * Adds attractions_email & panels_twilio_number vars
    
    * Updates to include tabletop twilio
    
    * Updates to include tabletop twilio

--- manifests/plugin_panels.pp
+++ manifests/plugin_panels.pp
@@ -14,6 +14,10 @@ class uber::plugin_panels (
   $panel_rooms = undef,
   $panel_app_deadline = undef,
   $alt_schedule_url = undef,
+  $attractions_email = undef,
+  $panels_twilio_number = undef,
+  $panels_twilio_sid = '',
+  $panels_twilio_token = '',
 ) {
   uber::repo { "${uber::plugins_dir}/panels":
     source   => $git_repo,
