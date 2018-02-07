fbd0c556e45d5594608578ba7b2dbaab7ae7041e
Rob Ruana <rob@robruana.com>
2018-02-07 12:08:26 -0500

    Preserves mivs game images before removing miss

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -77,13 +77,35 @@ class uber::plugins
       force   => true,
     }
 
-    # file {'remove_mivs':
-    #   ensure  => absent,
-    #   path    => "${uber::plugins_dir}/mivs",
-    #   recurse => true,
-    #   purge   => true,
-    #   force   => true,
-    # }
+    $mivs_screenshots_source = file("${uber::plugins_dir}/mivs/screenshots", '/dev/null')
+    if($mivs_screenshots_source != '') {
+      file { 'copy_mivs_screenshots':
+        ensure  => 'directory',
+        recurse => true,
+        path    => "${uber::plugins_dir}/uber/uploaded_files/mivs_game_images",
+        source  => "${uber::plugins_dir}/mivs/screenshots",
+        before  => File['remove_mivs'],
+      }
+    }
+
+    $mivs_game_images_source = file("${uber::plugins_dir}/mivs/uploaded_files/mivs_game_images", '/dev/null')
+    if($mivs_game_images_source != '') {
+      file { 'copy_mivs_game_images':
+        ensure  => 'directory',
+        recurse => true,
+        path    => "${uber::plugins_dir}/uber/uploaded_files/mivs_game_images",
+        source  => "${uber::plugins_dir}/mivs/uploaded_files/mivs_game_images",
+        before  => File['remove_mivs'],
+      }
+    }
+
+    file {'remove_mivs':
+      ensure  => absent,
+      path    => "${uber::plugins_dir}/mivs",
+      recurse => true,
+      purge   => true,
+      force   => true,
+    }
 
     # file {'remove_mits':
     #   ensure  => absent,
